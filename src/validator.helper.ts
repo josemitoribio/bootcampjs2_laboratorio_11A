f209c325fee116f15f1bb017af294458356b0134
Josemi Toribio <josemitoribio@gmail.com>
2024-02-19 13:00:18 +0100

    Laboratorio 11A finalizado

--- src/validator.helper.ts
+++ src/validator.helper.ts
@@ -47,14 +47,25 @@ export const extraerNombreBanco = (grupos: any): string => {
     }
 }
 
-export const extraerCodigoSucursal = (grupos: any): string => {
-    return grupos.codigoSucursal;
-}
+export const pintarMensajeOk = (ibanValue: any) => {
+    const patronIban = /^(?<codigoPais>[A-Z]{2})(?<digitoControl1>\d{2})(\s|-)?(?<codigoBanco>\d{4})(\s|-)?(?<codigoSucursal>\d{4})(\s|-)?(?<digitoControl>\d{2})(\s|-)?(?<numeroCuenta>\d{10})$/;
+    const coincidencia = patronIban.exec(ibanValue);
 
-export const extraerDigitoControl = (grupos: any): string => {
-    return grupos.digitoControl;
-}
+    if (coincidencia !== null && coincidencia !== undefined && typeof coincidencia.groups === 'object') {
+        const { codigoSucursal, digitoControl, numeroCuenta, codigoBanco } = coincidencia.groups;
+        const nombreBanco = extraerNombreBanco(codigoBanco);
+        const mensaje = `Banco: ${nombreBanco}\nCódigo de Sucursal: ${codigoSucursal}\nDigito de control: ${digitoControl}\nNúmero de cuenta: ${numeroCuenta}`;
 
-export const extraerNumeroCuenta = (grupos: any): string => {
-    return grupos.numeroCuenta;
-}
+        const resultadoElement = document.getElementById('resultado');
+        if (resultadoElement !== null && resultadoElement !== undefined && resultadoElement instanceof HTMLDivElement) {
+            resultadoElement.innerText = mensaje;
+        }
+    }
+};
+
+export const pintarMensajeError = () => {
+    const resultadoElement = document.getElementById('resultado');
+    if (resultadoElement !== null && resultadoElement !== undefined && resultadoElement instanceof HTMLDivElement) {
+        resultadoElement.innerText = `Este IBAN no es válido. Revíselo de nuevo.`;
+    }
+};
--- src/validator.ts
+++ src/validator.ts
@@ -1,35 +1,23 @@
 import {
-    extraerCodigoSucursal,
-    extraerDigitoControl,
-    extraerNombreBanco,
-    extraerNumeroCuenta
+    pintarMensajeOk,
+    pintarMensajeError
 } from "./validator.helper";
+
+import * as ibantools from 'ibantools';
 
 const validarIBAN = () => {
     const ibanInput = document.getElementById("ibanInput");
-    if (ibanInput !== null && ibanInput !== undefined && ibanInput instanceof HTMLInputElement) {
+    if (ibanInput !== null 
+        && ibanInput !== undefined 
+        && ibanInput instanceof HTMLInputElement) {
         const ibanValue = ibanInput.value;
         if (ibanValue) {
-            const patronIban = /^(?<codigoPais>[A-Z]{2})(?<digitoControl1>\d{2})(\s|-)?(?<codigoBanco>\d{4})(\s|-)?(?<codigoSucursal>\d{4})(\s|-)?(?<digitoControl>\d{2})(\s|-)?(?<numeroCuenta>\d{10})$/;
-            const coincidencia = patronIban.exec(ibanValue);
+            const isValid = ibantools.isValidIBAN(ibanValue);
 
-            if (coincidencia !== null && coincidencia !== undefined) {
-                const nombreBanco = extraerNombreBanco(coincidencia.groups);
-                const codigoSucursal = extraerCodigoSucursal(coincidencia.groups);
-                const digitoControl = extraerDigitoControl(coincidencia.groups);
-                const numeroCuenta = extraerNumeroCuenta(coincidencia.groups);
-
-                const mensaje = `Banco: ${nombreBanco}\nCódigo de Sucursal: ${codigoSucursal}\nDigito de control: ${digitoControl}\nNúmero de cuenta: ${numeroCuenta}`;
-
-                const resultadoElement = document.getElementById('resultado');
-                if (resultadoElement !== null && resultadoElement !== undefined) {
-                    resultadoElement.innerText = mensaje;
-                }
+            if (isValid) {
+                pintarMensajeOk(ibanValue);
             } else {
-                const resultadoElement = document.getElementById('resultado');
-                if (resultadoElement !== null && resultadoElement !== undefined) {
-                    resultadoElement.innerText = `Este IBAN no es válido. Revíselo de nuevo.`;
-                }
+                pintarMensajeError();
             }
         }
     }
